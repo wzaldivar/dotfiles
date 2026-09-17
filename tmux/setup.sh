68a8c2e554cfb4e46b9bbb924c649f8c2f2429c0
Walber Zaldivar Herrera <walber.zaldivar@gmail.com>
2026-09-17 15:42:15 -0300

    tmux setup installs catppuccin plugin and reloads config

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+mkdir -p ~/.config/tmux/plugins/catppuccin
+git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+
+tmux source ~/.tmux.conf
